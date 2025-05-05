522302e48591c4622cdc219454bee717a553d4e0
test <fanor.quintero@davivienda.com>
2025-05-05 13:36:40 -0500

    add: se sube base del proyecto

---
+++ src/test/java/com/careassistant/services/MsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careassistant.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
